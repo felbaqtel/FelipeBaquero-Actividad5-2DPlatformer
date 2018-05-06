ca5b81a3568eb1898d156fa8f8da16fbad1a3498
Felipe  Baquero <dubanbate@unisabana.edu.co>
2018-05-05 22:31:03 -0500

    version final

---
+++ Assets/Scripts/Movimiento.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movimiento : MonoBehaviour {
+
+
+	public float velX = 0.1f
+	public float movX;
+	public float inputX;
+
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		float inputX = Input.GetAxis ("Horizontal"); // Almacena movimiento en eje x
+
+		if (inputX > 0)
+			movX = transform.position.x + (inputX * velx);
+
+	}
+
+if (inputX < 0) 
+}
+
